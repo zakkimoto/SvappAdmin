19a49c44afff226cee4773782583547eddbbfcc4
Zakarías Friðriksson <zakariasfridriksson@Zakariass-MacBook-Pro.local>
2021-10-06 01:06:33 +0000

    message board með rétt flex zones núna

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -6,8 +6,20 @@
     </div>
     <div class="row-2">
       <div class="col-1">
+        <h1>Hér koma öll skilaboð</h1>
       </div>
       <div class="col-2">
+        <div class="col-3">
+          <h1>Zakarías Friðriksson</h1>
+        </div>
+        <div class="col-4">
+          <h1>haha</h1>
+        </div>
+        <div class="col-5">
+          <input type="text" placeholder="TEXTI" id="input"/>
+          <button type="button" id="button">Senda</button>
+          
+        </div>
       </div>
     </div>
     
@@ -29,9 +41,40 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+#input{
+  width: 90%;
+  border-radius:4px;
+  margin:8px auto;
+  outline:none;
+  padding:8px;
+  box-sizing:border-box;
+  transition:.3s;
+}
+
+#input:focus{
+  border-color:#A154F2;
+  box-shadow:0 0 8px 0 #A154F2;
+}
+
+#button{
+  width: 10%;
+  border-radius:4px;
+  margin:8px auto;
+  outline:none;
+  padding:8px;
+  box-sizing:border-box;
+  transition:.3s;
+}
+
+#button:focus{
+  border-color:#A154F2;
+  box-shadow:0 0 8px 0 #A154F2;
+}
+
+
 .container{
   display:flex;
-  justify-content: flex-end;
+  justify-content: flex-start;
   flex-direction: column;
   font-family: Montserrat;
 }
@@ -39,6 +82,7 @@ export default {
 .row-2{
   display:flex;
   flex-direction: row;
+  
 }
 
 .col-1{
@@ -52,10 +96,37 @@ export default {
   
 }
 .col-2{
+  display:flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
   width: 70%;
   height: 93vh;
   background-color: #30363D;
+  
 
+}
+
+.col-3{
+  width: 100%;
+  display:flex;
+}
+
+.col-4{
+  height: 90%;
+  width: 100%;
+  display:flex;
+  
+}
+.col-5{
+  
+  width: 100%;
+  display:flex;
+  align-items: end;
+  justify-content: end;
+  
+  
+  
 }
 
 </style>
